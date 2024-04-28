5d59f2ff5b575232513d3711b45fd489d71c4ddd
Ihor <ihor.bozhemskyy@gmail.com>
2024-04-27 19:08:54 -0500

    Loops example

---
+++ src/day01/Loops.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Loops {
+    public static void main(String[] args) {
+        int [] example = {3,43,32,5435, 34,23,1};
+        for (int i : example) {
+            System.out.println(i);
+
+        }
+    }
+}
