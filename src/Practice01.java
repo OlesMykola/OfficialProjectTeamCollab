0d3f1b28b1ecc9292b56c5e11c84c31877a264f1
home <untuzina92@gmail.com>
2024-04-27 20:18:37 -0500

    created first practice file

--- src/Practice01.java
+++ src/Practice01.java
@@ -3,6 +3,7 @@ public class Practice01 {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo friends!");
+        System.out.println("Good luck to everyone!");
 
     }
 }
